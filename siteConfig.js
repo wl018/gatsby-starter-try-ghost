797aa95202fff5cf6fcc128130644a6e5856e349
wl018 <76525568+wl018@users.noreply.github.com>
2020-12-28 19:37:50 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -10,13 +10,13 @@ module.exports = {
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `受苦与激情`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `在我们的世代传扬祂，见证祂。`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `受苦与激情`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
